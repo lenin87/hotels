22d7585aff6d881afbe225454f99d4f8e2ac53d9
lenin87 <lenin.komatipalli@gmail.com>
2018-07-24 15:21:52 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpModule} from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {MyserviceService} from './myservice.service';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
@@ -18,6 +19,7 @@ import { SqrtPipe } from './app.sqrt';
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     RouterModule.forRoot([{
       path:'new-cmp',
       component:NewCmpComponent
